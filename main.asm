8e4cae94c6007da2391debb8cdf78f83991f0ce5
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-16 21:42:50 -0700

    update gitignore

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	Author:				Sandro Aguilar
 ;	Date:				June 16, 2019
-;	Description:		Simple MASM program th
+;	Description:		Simple MASM program tha
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .386

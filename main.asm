b3c07605c48b9171b14af3942ad1b8ed23691fe5
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-16 21:41:41 -0700

    update gitignore

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	Author:				Sandro Aguilar
 ;	Date:				June 16, 2019
-;	Description:		Simple
+;	Description:		Simple M
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .386
